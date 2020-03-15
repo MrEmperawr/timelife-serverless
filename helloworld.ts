cbef4f8c55411a2b55d98d604676c3f3ceeede38
Marcus <marcus.kalmar@gmail.com>
2020-03-15 18:42:05 +0100

    Add babel setup

--- helloworld.ts
+++ helloworld.ts
@@ -2,7 +2,7 @@ import { IHelloWorld } from './types';
 
 
 export function handler (helloWorld: IHelloWorld): string {
-    const helloWorldString = `${helloWorld.greeting} ${helloWorld.phrase} ${helloWorld.number}`;
+    const helloWorldString = `${helloWorld.greeting} ${helloWorld.phrase} This is a good number: ${helloWorld.number}`;
     for (const word of helloWorld.words) {
         helloWorldString.toLocaleUpperCase + ' ' + word;
     }
@@ -24,4 +24,9 @@ export function createHelloWorld(
     }
     return helloWorld;
 }
-
+const helloWorld = {
+    greeting: "Hello y'all, I'm just a piece of shit!",
+    phrase: "I inveted magic among other things. Also I am basically god lol :>",
+    number: 1337,
+    words: ["I", "am", "really", "mediocre", "lawl."]
+}
